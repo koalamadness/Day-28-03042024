69c275a0ef12f5981b413061a22d209ad5dc6465
YeQiu <paulhiram@hotmail.com>
2024-04-04 20:04:51 +0000

    Added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
 
+    Example exa = new Example();
+
     ArrayList<Object> listaObjetos = new ArrayList<Object>();
 
       listaObjetos.add(true);
       listaObjetos.add(2);
       listaObjetos.add(2.64f);
       listaObjetos.add("Carlos");
+      listaObjetos.add(exa);
 
     System.out.println("Contenido del ArrayList:");
     for (Object objeto : listaObjetos) {
@@ -19,6 +22,12 @@ public class Main {
           System.out.println("The object: " + objeto + " is a String");
       } else if (objeto instanceof Integer) {
         System.out.println("The object: " + objeto + " is an Integer");
+      } else if (objeto instanceof Float ) {
+        System.out.println("The object: " + objeto + " is a Float");
+      } else if (objeto instanceof Boolean){
+        System.out.println("The object: " + objeto + " is a Boolean");
+      } else {
+        System.out.println("The object: " + objeto + " is an unidentified object");
       }
     }
   }
