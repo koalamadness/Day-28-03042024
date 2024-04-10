09799f1d8baad70cd84fa2838757862e864fd7d1
YeQiu <paulhiram@hotmail.com>
2024-04-10 19:24:00 +0000

    Anagram class created

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Anagram{
+   public HashMap<String, ArrayList<String>> anagrams = new HashMap<String, ArrayList<String>>();
+   public ArrayList<String> nombres = new ArrayList<String>();
+    
+ }
